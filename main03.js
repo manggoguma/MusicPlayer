6b3e79c0836856c9192cdbab82f58ea0532ef55b
haeunNam <nhe0103@gamail.com>
2024-02-29 17:31:24 +0900

    k-pop

--- main03.js
+++ main03.js
@@ -1,9 +1,8 @@
-let token ;
+let token = "BQBfgysxEW7t5vk5o7rMXjkSaKlBcNAR85AQq1dUhpDvx3CMAG8EexRW3tIJpfApB2CPQ-ZFOufQn_R3IXPdGb8BHcRi_oB8FiNBzDKp0SVsDCu5O4XRZgV6JziQEgj8GjfL6QFa6rpaJPyBtxfsIdBl_Sem2VfDkugDANXjJG80yC13wXvFK2V3wgKdH0XSzr4N3JMawzWSajqJtpo1C1NfmLfC";
 const client_id = "31fc18db9a8f46e7b0f17f84a143787d";
 const client_secret = "bbfa00cb2eb0482b98cc830df3de45b0";
 const redirect_uri = "http://localhost:5500/callback";
 
-let albums=[];
 let tracks=[];
 
 // 토큰 요청
@@ -55,59 +54,6 @@ const getData = async (url) => {
     }
 };
 
-// 최신앨범 가져오기
-const getNewAlbum = async () => {
-    const url = new URL(`https://api.spotify.com/v1/browse/new-releases?country=KR&limit=6`);
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
-    const data = await response.json();
-    console.log('zzzzzzz',data)
-    albums = data.albums.items;
-    render();    
-};
-
-// 더보기 함수
-let folderMore = false;
-
-const openMore = async () => {
-    const url = new URL(`https://api.spotify.com/v1/browse/new-releases?country=KR&limit=20`);
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
-    const data = await response.json();
-    albums = data.albums.items;
-    render();
-    folderMore=true;
-};
-
-// 기존 6개만 다시 보여주기
-const returnOri = async () => {
-    const url = new URL(`https://api.spotify.com/v1/browse/new-releases?country=KR&limit=6`);
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
-    const data = await response.json();
-    albums = data.albums.items;
-    render();
-    folderMore = false;
-};
-
-const toggleMore = () => {
-    if (folderMore) {
-        returnOri();
-    } else {
-        openMore();
-    }
-    folderMore = !folderMore;
-};
-
 let spoty_kr_category = `https://api.spotify.com/v1/recommendations?market=KR&seed_genres=k-pop`;
 
 // K-pop 플레이리스트 가져오기
@@ -130,32 +76,33 @@ const getRecommend = async () => {
 
     // data2.tracks.items를 반환하여 다른 곳에서 사용할 수 있도록 함
     return data2.tracks.items;
+    render02();
 }
 
+// 토큰 요청 후 앨범 정보 가져오기
 getToken().then(() => {
-    getNewAlbum();
-    getRecommend();
+    getRecommend().then((tracks) => {
+        // tracks 배열에 있는 각 곡의 정보를 사용할 수 있음
+        console.log(tracks);
+    });
 });
 
-const render = () => {
-    const newAlbumHTML = albums.map((item) => {
-        const artists = item.artists.map(artist => artist.name).join(', ');
+
+const render02 = () => {
+    const recommendListHTML = tracks.map((track) => {
+        const artists = track.artists.map(artist => artist.name).join(', ');
         return `<div class="row">
             <div class="col">
                 <div class="col-lg-8">
-                    <img class="album-img-size" src=${item.images[0].url} alt="">
+                    <img class="album-img-size" src=${track.album.images[0].url} alt="">
                 </div>
                 <div class="col-lg-4">
-                    <h5>${item.name}</h5>
+                    <h5>${track.name}</h5>
                     <p>${artists}</p>
                 </div>
             </div>
         </div>`;
     });
 
-    document.getElementById("lowerBar").innerHTML = newAlbumHTML.join('');
+    document.getElementById("lowerBar02").innerHTML = recommendListHTML.join('');
 }
-
-
-
-
