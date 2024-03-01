00928ae5cef5a6df50a0f1ffd541216a4360ca63
SpringDream0406 <SpringDream0406@gmail.com>
2024-03-01 22:08:51 +0900

    일단 조금 바꿈

--- main02.js
+++ main02.js
@@ -9,140 +9,150 @@ let folderMore = false;
 
 // 토큰 요청
 const getToken = async () => {
-    const authOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: "Basic " + btoa(client_id + ":" + client_secret),
-        },
-        body: "grant_type=client_credentials",
-    };
+  const authOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(client_id + ":" + client_secret),
+    },
+    body: "grant_type=client_credentials",
+  };
 
-    try {
-        const response = await fetch(
-            "https://accounts.spotify.com/api/token",
-            authOptions
-        );
-        token = await response.json();
-    } catch (error) {
-        console.error("Error:", error);
-    }
+  try {
+    const response = await fetch(
+      "https://accounts.spotify.com/api/token",
+      authOptions
+    );
+    token = await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
 
 // 노래 데이터 요청
 let spoty_url = `https://api.spotify.com/v1/search?`;
 
 const getData = async (url) => {
-    // 토큰이 없을 경우 token 요청
-    if (!token) {
-        await getToken();
+  // 토큰이 없을 경우 token 요청
+  if (!token) {
+    await getToken();
+  }
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    // 토큰이 만료되어 401 에러가 날 경우 토큰 다시 요청 하고 getData 다시 수행 getData는 필요에 맞춰 변경해야할듯
+    if (error.status === 401) {
+      await getToken();
+      return getData();
+    } else {
+      console.error("Error:", error);
     }
-    try {
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${token.access_token}`,
-            },
-        });
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        // 토큰이 만료되어 401 에러가 날 경우 토큰 다시 요청 하고 getData 다시 수행 getData는 필요에 맞춰 변경해야할듯
-        if (error.status === 401) {
-            await getToken();
-            return getData();
-        } else {
-            console.error("Error:", error);
-        }
-    }
+  }
 };
 
 // 최신앨범 가져오기
 const getNewAlbum = async () => {
-    const url = new URL(
-        `https://api.spotify.com/v1/browse/new-releases?country=KR&limit=6`
-    );
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
-    const data = await response.json();
-    console.log("zzzzzzz", data);
-    albums = data.albums.items;
-    render();
+  if (!token) {
+    await getToken();
+  }
+  const url = new URL(
+    `https://api.spotify.com/v1/browse/new-releases?country=KR&limit=6`
+  );
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  });
+  const data = await response.json();
+  console.log("zzzzzzz", data);
+  albums = data.albums.items;
+  render();
 };
 
 // 더보기 함수(최신)
 
 const openMore = async () => {
-    const url = new URL(
-        `https://api.spotify.com/v1/browse/new-releases?country=KR&limit=20`
-    );
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
-    const data = await response.json();
-    albums = data.albums.items;
-    render();
-    folderMore = true;
+  if (!token) {
+    await getToken();
+  }
+  const url = new URL(
+    `https://api.spotify.com/v1/browse/new-releases?country=KR&limit=20`
+  );
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  });
+  const data = await response.json();
+  albums = data.albums.items;
+  render();
+  folderMore = true;
 };
 
 // 기존 6개만 다시 보여주기(최신)
 const returnOri = async () => {
-    const url = new URL(
-        `https://api.spotify.com/v1/browse/new-releases?country=KR&limit=6`
-    );
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
-    const data = await response.json();
-    albums = data.albums.items;
-    render();
-    folderMore = false;
+  if (!token) {
+    await getToken();
+  }
+  const url = new URL(
+    `https://api.spotify.com/v1/browse/new-releases?country=KR&limit=6`
+  );
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  });
+  const data = await response.json();
+  albums = data.albums.items;
+  render();
+  folderMore = false;
 };
 
 const toggleMore = () => {
-    if (folderMore) {
-        returnOri();
-    } else {
-        openMore();
-    }
-    folderMore = !folderMore;
+  if (folderMore) {
+    returnOri();
+  } else {
+    openMore();
+  }
+  folderMore = !folderMore;
 };
 
 let spoty_kr_category = `https://api.spotify.com/v1/recommendations?market=KR&seed_genres=k-pop&limit=6`;
 
 // K-pop 플레이리스트 가져오기
 const getRecommend = async () => {
-    const url = new URL(`${spoty_kr_category}`);
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    });
+  if (!token) {
+    await getToken();
+  }
+  const url = new URL(`${spoty_kr_category}`);
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  });
 
-    const data2 = await response.json();
-    tracks = data2.tracks;
-    console.log("왜 안나와?", tracks);
+  const data2 = await response.json();
+  tracks = data2.tracks;
+  console.log("왜 안나와?", tracks);
 
-    // tracks 배열에서 각 곡의 name 속성을 가져오기
-    const trackNames = tracks.map((track) => track.name);
+  // tracks 배열에서 각 곡의 name 속성을 가져오기
+  const trackNames = tracks.map((track) => track.name);
 
-    // trackNames 배열에는 tracks 배열의 각 요소에서 name 속성만을 추출한 배열이 저장됩니다.
-    console.log("노래제목", trackNames);
+  // trackNames 배열에는 tracks 배열의 각 요소에서 name 속성만을 추출한 배열이 저장됩니다.
+  console.log("노래제목", trackNames);
 
-    // data2.tracks.items를 반환하여 다른 곳에서 사용할 수 있도록 함
-    return data2.tracks.items;
+  // data2.tracks.items를 반환하여 다른 곳에서 사용할 수 있도록 함
+  return data2.tracks.items;
 };
 
-getToken().then(() => {
-    getNewAlbum();
-    getRecommend();
-});
+// getNewAlbum();
+getRecommend();
 
 // // k-pop 더보기
 // const recommendMore = async () => {
@@ -186,11 +196,10 @@ getToken().then(() => {
 //     folderMore = !folderMore;
 // };
 
-
 const render = () => {
-    const newAlbumHTML = albums.map((item) => {
-        const artists = item.artists.map((artist) => artist.name).join(", ");
-        return `<div class="row">
+  const newAlbumHTML = albums.map((item) => {
+    const artists = item.artists.map((artist) => artist.name).join(", ");
+    return `<div class="row">
             <div class="col">
                 <div class="col-lg-8">
                     <img class="album-img-size" src=${item.images[0].url} alt="">
@@ -201,13 +210,13 @@ const render = () => {
                 </div>
             </div>
         </div>`;
-    });
+  });
 
-    document.getElementById("lowerBar").innerHTML = newAlbumHTML.join("");
+  document.getElementById("lowerBar").innerHTML = newAlbumHTML.join("");
 
-    const recommendLIstHTML = tracks.map((track) => {
-        const artists = track.artists.map((artist) => artist.name).join(", ");
-        return `<div id="recommendLowerBa">
+  const recommendLIstHTML = tracks.map((track) => {
+    const artists = track.artists.map((artist) => artist.name).join(", ");
+    return `<div id="recommendLowerBa">
         <div class="row">
             <div class="col">
                 <div class="col-lg-8">
@@ -220,12 +229,8 @@ const render = () => {
             </div>
         </div>
     </div>`;
-    });
+  });
 
-    document.getElementById("recommendLowerBar").innerHTML = recommendLIstHTML.join("");
+  document.getElementById("recommendLowerBar").innerHTML =
+    recommendLIstHTML.join("");
 };
-
-
-
-
-
