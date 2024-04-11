888a12bafbf52cfe6eaa29f65179367b377e1626
SpringDream0406 <springdream0406@gmail.com>
2024-04-11 15:58:50 +0900

    test

--- keys.js
+++
@@ -1,8 +0,0 @@
-export const keys = {
-  // client_id: "027d68ef08f84e8ebbf9e24aa91a52b5",
-  // client_secret: "6c38e73980514a03bc9b3be8da228ca5",
-  // client_id: "31fc18db9a8f46e7b0f17f84a143787d",
-  // client_secret: "bbfa00cb2eb0482b98cc830df3de45b0",
-  client_id: "5f95a65eea5e4e779fdeb5b620d39f5c",
-  client_secret: "267919ed9b2a48c38bac5eea4fe9b4fc",
-};
