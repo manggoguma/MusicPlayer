682b3a9d4a7de8a1f1bdf6671745ddb20c31d85f
SpringDream0406 <springdream0406@gmail.com>
2024-05-19 21:45:36 +0900

    api_url 변경

--- modules/getData.js
+++ modules/getData.js
@@ -61,7 +61,7 @@
 // };
 
 const TEST_URL = "http://localhost:3001/getData/";
-const API_URL = "43.203.95.172";
+const API_URL = "http://43.203.95.172/getData";
 const spotifyURL = `https://api.spotify.com/v1/`;
 
 export const getData = async (url) => {
