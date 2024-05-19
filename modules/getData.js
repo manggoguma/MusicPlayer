aba5194e1ae6b8ef4f9f29d1836fced7a1c927cc
SpringDream0406 <springdream0406@gmail.com>
2024-05-19 22:24:31 +0900

    redirects

--- modules/getData.js
+++ modules/getData.js
@@ -61,7 +61,7 @@
 // };
 
 const TEST_URL = "http://localhost:3001/getData/";
-const API_URL = "http://43.203.95.172/getData";
+const API_URL = "https://noona-music.netlify.app/getData";
 const spotifyURL = `https://api.spotify.com/v1/`;
 
 export const getData = async (url) => {
