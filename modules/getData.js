b2b4a901c700b659b5c80156dd45a044b1250d49
SpringDream0406 <springdream0406@gmail.com>
2024-07-31 23:54:27 +0900

    test

--- modules/getData.js
+++ modules/getData.js
@@ -1,82 +1,39 @@
-// import { keys } from "../keys.js";
-// let { client_id, client_secret } = keys;
+let spotifyURL = `https://api.spotify.com/v1/`;
 
-// let spotifyURL = `https://api.spotify.com/v1/`;
+let token;
 
-// // 토큰 요청
-// const getToken = async ({ client_id, client_secret }) => {
-//   const authOptions = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/x-www-form-urlencoded",
-//       Authorization: "Basic " + btoa(client_id + ":" + client_secret),
-//     },
-//     body: "grant_type=client_credentials",
-//   };
-
-//   try {
-//     const response = await fetch(
-//       "https://accounts.spotify.com/api/token",
-//       authOptions
-//     );
-//     // 토큰 반환
-//     let token = await response.json();
-//     return token;
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// };
-
-// let token;
-
-// // 데이터 요청
-// export const getData = async (url) => {
-//   // 토큰이 없을 경우 token 요청
-//   if (!token) {
-//     token = await getToken({ client_id, client_secret });
-//   }
-//   try {
-//     const response = await fetch(url, {
-//       headers: {
-//         Authorization: `Bearer ${token.access_token}`,
-//       },
-//     });
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     // 토큰이 만료되어 401 에러가 날 경우 토큰 다시 요청 하고 getData 다시 수행
-//     if (error.status === 401) {
-//       await getToken({ client_id, client_secret });
-//       return getData(url);
-//     } else {
-//       console.error("Error:", error);
-//     }
-//   }
-// };
-
-// // 검색용 함수
-// export const spotifySearch = ({ q, type }) => {
-//   let url = `${spotifyURL}search?q=${q}&type=${type}&limit=6`;
-//   return getData(url);
-// };
-
-const TEST_URL = "http://localhost:3001/getData/";
-const API_URL = "api/getData";
-const spotifyURL = `https://api.spotify.com/v1/`;
-
-export const getData = async (url) => {
+// 토큰 요청 함수
+const getToken = async () => {
   try {
-    const response = await fetch(API_URL, {
-      method: "POST",
+    const reponse = await fetch("./netlify/function/getToken");
+    token = await reponse.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 데이터 요청
+export const getData = async (url) => {
+  // 토큰이 없을 경우 token 요청
+  if (!token) {
+    await getToken();
+  }
+  try {
+    const response = await fetch(url, {
       headers: {
-        "Content-Type": "application/json",
+        Authorization: `Bearer ${token.access_token}`,
       },
-      body: JSON.stringify({ url: url }),
     });
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log(error);
+    // 토큰이 만료되어 401 에러가 날 경우 토큰 다시 요청 하고 getData 다시 수행
+    if (error.status === 401) {
+      await getToken();
+      return getData(url);
+    } else {
+      console.error("Error:", error);
+    }
   }
 };
 
@@ -85,3 +42,31 @@ export const spotifySearch = ({ q, type }) => {
   let url = `${spotifyURL}search?q=${q}&type=${type}&limit=6`;
   return getData(url);
 };
+
+//
+// ========== 요청 서버 따로 두는 경우 ==========
+// const TEST_URL = "http://localhost:3001/getData/";
+// const API_URL = "api/getData";
+// const spotifyURL = `https://api.spotify.com/v1/`;
+
+// export const getData = async (url) => {
+//   try {
+//     const response = await fetch(API_URL, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ url: url }),
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// // 검색용 함수
+// export const spotifySearch = ({ q, type }) => {
+//   let url = `${spotifyURL}search?q=${q}&type=${type}&limit=6`;
+//   return getData(url);
+// };
---
+++ netlify-functions/getToken.js
@@ -0,0 +1,39 @@
+const fetch = require("node-fetch");
+const querystring = require("querystring");
+
+const encodeCredentials = (clientId, clientSecret) => {
+  return Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+};
+
+exports.handler = async (event, context) => {
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  const authOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${encodeCredentials(client_id, client_secret)}`,
+    },
+    body: querystring.stringify({ grant_type: "client_credentials" }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://accounts.spotify.com/api/token",
+      authOptions
+    );
+    if (!response.ok) throw new Error("Failed to fetch token");
+    const token = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(token),
+    };
+  } catch (error) {
+    console.error("Error fetching token:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
